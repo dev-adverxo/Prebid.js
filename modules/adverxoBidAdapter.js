097a2a8079faab3affed2f0791c6b9b25ee0137a
Rolando <rolando.ramirez@adverxo.com>
2024-10-21 09:59:45 +0200

    Test checkpoint

--- modules/adverxoBidAdapter.js
+++ modules/adverxoBidAdapter.js
@@ -2,8 +2,8 @@ import * as utils from 'src/utils';
 import {config} from 'src/config';
 import {registerBidder} from 'src/adapters/bidderFactory';
 import {BANNER, VIDEO, NATIVE} from 'src/mediaTypes.js';
-import { ortbConverter as OrtbConverter } from '../libraries/ortbConverter/converter.js';
-import { Renderer } from '../src/Renderer.js';
+import {ortbConverter as OrtbConverter} from '../libraries/ortbConverter/converter.js';
+import {Renderer} from '../src/Renderer.js';
 
 const BIDDER_CODE = 'adverxo';
 const GVLID = 0; // TODO, NoCommit, 9/10/24: Ponerlo
@@ -84,7 +84,7 @@ const videoUtils = {
 
       win.ANOutstreamVideo.renderAd({
         targetId: bid.adUnitCode,
-        adResponse: { content: bid.vastXml }
+        adResponse: {content: bid.vastXml}
       });
     });
   }
@@ -131,197 +131,197 @@ const userSyncUtils = {
 };
 
 const adverxoUtils = {
-    /**
-     * Build auctionUrl for AdUnit
-     *
-     * @param {number} adUnitId
-     * @param {string} adUnitAuth
-     * @returns string
-     */
-    buildAuctionUrl: function(adUnitId, adUnitAuth) {
-      return ENDPOINT_URL
-          .replace(ENDPOINT_URL_AD_UNIT_PLACEHOLDER, adUnitId)
-          .replace(ENDPOINT_URL_AUTH_PLACEHOLDER, adUnitAuth);
-    },
+  /**
+   * Build auctionUrl for AdUnit
+   *
+   * @param {number} adUnitId
+   * @param {string} adUnitAuth
+   * @returns string
+   */
+  buildAuctionUrl: function (adUnitId, adUnitAuth) {
+    return ENDPOINT_URL
+      .replace(ENDPOINT_URL_AD_UNIT_PLACEHOLDER, adUnitId)
+      .replace(ENDPOINT_URL_AUTH_PLACEHOLDER, adUnitAuth);
+  },
 
-    /**
-     * Extract userSync from ortb response
-     *
-     * @param {ServerResponse} serverResponse
-     * @return {UserSync[]} The user syncs which should be dropped.
-     */
-    extractUserSyncFromResponse: function(serverResponse) {
-      const userSyncs = (serverResponse?.body?.ext?.avx_usersync || []);
-      return userSyncs.map(({url, type}) => ({type: PBS_SYNC_TYPES[type], url: url}))
-    },
+  /**
+   * Extract userSync from ortb response
+   *
+   * @param {ServerResponse} serverResponse
+   * @return {UserSync[]} The user syncs which should be dropped.
+   */
+  extractUserSyncFromResponse: function (serverResponse) {
+    const userSyncs = (serverResponse?.body?.ext?.avx_usersync || []);
+    return userSyncs.map(({url, type}) => ({type: PBS_SYNC_TYPES[type], url: url}))
+  },
 
-    /**
-     * Create user privacy regulations object
-     * @param bidderRequest {BidderRequest}
-     * @returns {{regs: Object}} ortb regulations object
-     */
-    buildOrtbRegulations: function (bidderRequest) {
-      const {gdprConsent, uspConsent, gppConsent} = bidderRequest;
-      const ext = {};
+  /**
+   * Create user privacy regulations object
+   * @param bidderRequest {BidderRequest}
+   * @returns {{regs: Object}} ortb regulations object
+   */
+  buildOrtbRegulations: function (bidderRequest) {
+    const {gdprConsent, uspConsent, gppConsent} = bidderRequest;
+    const ext = {};
 
-      if (gdprConsent) {
-        ext.gdpr = Number(gdprConsent.gdprApplies);
-        ext.gdpr_consent = gdprConsent.consentString;
-      }
-
-      if (gppConsent) {
-        ext.gpp = gppConsent.gppString;
-        ext.gpp_sid = gppConsent.applicableSections;
-      }
-
-      if (uspConsent) {
-        ext.us_privacy = uspConsent;
-      }
-
-      return {
-        coppa: config.getConfig('coppa') ? 1 : 0,
-        ext
-      };
-    },
-
-    /**
-     * Build user sync method info
-     * @param bidderRequest {BidderRequest}
-     * @returns {int|null} userSync method
-     */
-    getAllowedUserSyncMethod: function (bidderRequest) {
-      const {bidderCode} = bidderRequest;
-      return userSyncUtils.getAllowedSyncMethod(bidderCode);
-    },
-
-    /**
-     * Group given array of bidRequests by params.publisher
-     *
-     * @param {BidRequest[]} bidRequests
-     * @returns {Map.<Object, BidRequest[]>}
-     */
-    groupBidRequestsByAdUnit: function (bidRequests) {
-      const groupedBidRequests = new Map();
-
-      bidRequests.forEach(bidRequest => {
-        const adUnit = {
-          id: bidRequest.params.adUnitId,
-          auth: bidRequest.params.auth
-        };
-
-        if (!groupedBidRequests.get(adUnit)) {
-          groupedBidRequests.set(adUnit, []);
-        }
-
-        groupedBidRequests.get(adUnit).push(bidRequest);
-      });
-
-      return groupedBidRequests;
+    if (gdprConsent) {
+      ext.gdpr = Number(gdprConsent.gdprApplies);
+      ext.gdpr_consent = gdprConsent.consentString;
     }
+
+    if (gppConsent) {
+      ext.gpp = gppConsent.gppString;
+      ext.gpp_sid = gppConsent.applicableSections;
+    }
+
+    if (uspConsent) {
+      ext.us_privacy = uspConsent;
+    }
+
+    return {
+      coppa: config.getConfig('coppa') ? 1 : 0,
+      ext
+    };
+  },
+
+  /**
+   * Build user sync method info
+   * @param bidderRequest {BidderRequest}
+   * @returns {int|null} userSync method
+   */
+  getAllowedUserSyncMethod: function (bidderRequest) {
+    const {bidderCode} = bidderRequest;
+    return userSyncUtils.getAllowedSyncMethod(bidderCode);
+  },
+
+  /**
+   * Group given array of bidRequests by params.publisher
+   *
+   * @param {BidRequest[]} bidRequests
+   * @returns {Map.<Object, BidRequest[]>}
+   */
+  groupBidRequestsByAdUnit: function (bidRequests) {
+    const groupedBidRequests = new Map();
+
+    bidRequests.forEach(bidRequest => {
+      const adUnit = {
+        id: bidRequest.params.adUnitId,
+        auth: bidRequest.params.auth
+      };
+
+      if (!groupedBidRequests.get(adUnit)) {
+        groupedBidRequests.set(adUnit, []);
+      }
+
+      groupedBidRequests.get(adUnit).push(bidRequest);
+    });
+
+    return groupedBidRequests;
+  }
 };
 
 export const spec = {
-    code: BIDDER_CODE,
-    gvlid: GVLID,
-    supportedMediaTypes: [BANNER, VIDEO, NATIVE],
-    aliases: [],
+  code: BIDDER_CODE,
+  gvlid: GVLID,
+  supportedMediaTypes: [BANNER, NATIVE, VIDEO],
+  aliases: [],
 
-    /**
-     * Determines whether or not the given bid request is valid.
-     *
-     * @param {BidRequest} bid The bid params to validate.
-     * @return boolean True if this is a valid bid, and false otherwise.
-     */
-    isBidRequestValid: function(bid) {
-        if (!utils.isPlainObject(bid.params) || !Object.keys(bid.params).length) {
-          utils.logWarn('Adverxo Bid Adapter: bid params must be provided.');
-          return false;
-        }
+  /**
+   * Determines whether or not the given bid request is valid.
+   *
+   * @param {BidRequest} bid The bid params to validate.
+   * @return boolean True if this is a valid bid, and false otherwise.
+   */
+  isBidRequestValid: function (bid) {
+    if (!utils.isPlainObject(bid.params) || !Object.keys(bid.params).length) {
+      utils.logWarn('Adverxo Bid Adapter: bid params must be provided.');
+      return false;
+    }
 
-        if (!bid.params.adUnitId || typeof bid.params.adUnitId !== 'number') {
-          utils.logWarn('Adverxo Bid Adapter: adUnitId bid param is required and must be a number');
-          return false;
-        }
+    if (!bid.params.adUnitId || typeof bid.params.adUnitId !== 'number') {
+      utils.logWarn('Adverxo Bid Adapter: adUnitId bid param is required and must be a number');
+      return false;
+    }
 
-        if (!bid.params.adUnitId || typeof bid.params.auth !== 'string') {
-          utils.logWarn('Adverxo Bid Adapter: auth bid param is required and must be a string');
-          return false;
-        }
+    if (!bid.params.adUnitId || typeof bid.params.auth !== 'string') {
+      utils.logWarn('Adverxo Bid Adapter: auth bid param is required and must be a string');
+      return false;
+    }
 
-        return true;
-    },
+    return true;
+  },
 
-    /**
-     * Make a server request from the list of BidRequests.
-     *
-     * @param {validBidRequests[]} - an array of bids
-     * @return ServerRequest Info describing the request to the server.
-     */
-    buildRequests: function(validBidRequests, bidderRequest) {
-      const result = [];
+  /**
+   * Make a server request from the list of BidRequests.
+   *
+   * @param {validBidRequests[]} - an array of bids
+   * @return ServerRequest Info describing the request to the server.
+   */
+  buildRequests: function (validBidRequests, bidderRequest) {
+    const result = [];
 
-      const bidRequestsByAdUnit = adverxoUtils.groupBidRequestsByAdUnit(validBidRequests);
+    const bidRequestsByAdUnit = adverxoUtils.groupBidRequestsByAdUnit(validBidRequests);
 
-      bidRequestsByAdUnit.forEach((adUnitBidRequests, adUnit) => {
-        const ortbRequest = ortbConverter.toORTB({
-          bidRequests: adUnitBidRequests,
-          bidderRequest
-        });
-
-        result.push({
-           method: 'POST',
-           url: adverxoUtils.buildAuctionUrl(adUnit.id, adUnit.auth),
-           data: ortbRequest,
-           bids: adUnitBidRequests
-        });
+    bidRequestsByAdUnit.forEach((adUnitBidRequests, adUnit) => {
+      const ortbRequest = ortbConverter.toORTB({
+        bidRequests: adUnitBidRequests,
+        bidderRequest
       });
 
-      return result;
-    },
+      result.push({
+        method: 'POST',
+        url: adverxoUtils.buildAuctionUrl(adUnit.id, adUnit.auth),
+        data: ortbRequest,
+        bids: adUnitBidRequests
+      });
+    });
 
-    /**
-     * Unpack the response from the server into a list of bids.
-     *
-     * @param {ServerResponse} serverResponse A successful response from the server.
-     * @return {Bid[]} An array of bids which were nested inside the server.
-     */
-    interpretResponse: function(serverResponse, bidRequest) {
-      if (!serverResponse || !bidRequest) {
-        return [];
+    return result;
+  },
+
+  /**
+   * Unpack the response from the server into a list of bids.
+   *
+   * @param {ServerResponse} serverResponse A successful response from the server.
+   * @return {Bid[]} An array of bids which were nested inside the server.
+   */
+  interpretResponse: function (serverResponse, bidRequest) {
+    if (!serverResponse || !bidRequest) {
+      return [];
+    }
+
+    const bids = ortbConverter.fromORTB({
+      response: serverResponse.body,
+      request: bidRequest.data,
+    }).bids;
+
+    return bids.map((bid) => {
+      const thisRequest = utils.getBidRequest(bid.requestId, [bidRequest]);
+      const context = utils.deepAccess(thisRequest, 'mediaTypes.video.context');
+
+      if (bid.mediaType === 'video' && context === 'outstream') {
+        bid.renderer = videoUtils.createOutstreamVideoRenderer(bid);
       }
 
-      const bids = ortbConverter.fromORTB({
-        response: serverResponse.body,
-        request: bidRequest.data,
-      }).bids;
+      return bid;
+    });
+  },
 
-      return bids.map((bid) => {
-        const thisRequest = utils.getBidRequest(bid.requestId, [bidRequest]);
-        const context = utils.deepAccess(thisRequest, 'mediaTypes.video.context');
-
-        if (bid.mediaType === 'video' && context === 'outstream') {
-          bid.renderer = videoUtils.createOutstreamVideoRenderer(bid);
-        }
-
-        return bid;
-      });
-    },
-
-    /**
-     * Register the user sync pixels which should be dropped after the auction.
-     *
-     * @param {SyncOptions} syncOptions Which user syncs are allowed?
-     * @param {ServerResponse[]} serverResponses List of server's responses.
-     * @return {UserSync[]} The user syncs which should be dropped.
-     */
-    getUserSyncs: function(syncOptions, serverResponses) {
-        if (!serverResponses?.length || (!syncOptions.iframeEnabled && !syncOptions.pixelEnabled)) {
-          return [];
-        }
-
-        return serverResponses.map(serverResponse => adverxoUtils.extractUserSyncFromResponse(serverResponse))
-          .flat();
+  /**
+   * Register the user sync pixels which should be dropped after the auction.
+   *
+   * @param {SyncOptions} syncOptions Which user syncs are allowed?
+   * @param {ServerResponse[]} serverResponses List of server's responses.
+   * @return {UserSync[]} The user syncs which should be dropped.
+   */
+  getUserSyncs: function (syncOptions, serverResponses) {
+    if (!serverResponses?.length || (!syncOptions.iframeEnabled && !syncOptions.pixelEnabled)) {
+      return [];
     }
+
+    return serverResponses.map(serverResponse => adverxoUtils.extractUserSyncFromResponse(serverResponse))
+      .flat();
+  }
 }
 
 registerBidder(spec);
